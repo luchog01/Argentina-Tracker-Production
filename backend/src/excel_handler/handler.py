c443a12ed101c32e794f76c0d46a142c3ebd2a77
Luciano Garbarino <lucigar01@gmail.com>
2023-01-26 11:22:50 -0300

    v3.0

--- backend/src/excel_handler/handler.py
+++ backend/src/excel_handler/handler.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import json
 
 files_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "files")
 
@@ -11,23 +12,35 @@ def get_excel(ticker: int) -> str:
     return file_path
 
 def update_excel(funds_data: dict, fecha: str) -> None:
+
     for key in funds_data.keys():
         file_path = os.path.join(files_folder,f"{key}.xlsx")
         try:
             old_df = pd.read_excel(file_path, index_col=0)
         except FileNotFoundError:
             old_df = pd.DataFrame()
-
         update_df = funds_data[key]
         update_df = pd.DataFrame(update_df)
         update_df.drop("price", axis=0, inplace=True)
         update_df = update_df.transpose()
         update_df.rename(columns={"qty": fecha}, inplace=True)
-
         try:
-            update_df = old_df.join(update_df, how="outer", sort=True)
+            if fecha in list(old_df.columns):
+                old_df.drop(fecha, axis=1, inplace=True) # Si la fecha ya existe, la borra
             
+            update_df = old_df.join(update_df, how="outer", sort=True) # Une los dataframes
+        
             update_df.to_excel(file_path)
-            print("[OK] [file_handler.py] Excel saved")
+            print(f"[OK] [file_handler.py] [{key}] Excel saved")
         except ValueError:
-            print("[ERROR] [file_handler.py] ValueError when joining dataframes")
+            print(f"[ERROR] [file_handler.py] [{key}] ValueError when joining dataframes")
+        except Exception as e:
+            print(f"[ERROR] [file_handler.py] [{key}] {e}")
+
+
+
+# with open (os.path.join(files_folder,"borrar.txt"), "r") as f:
+#         data = f.read()
+#         data = json.loads(data)
+
+# update_excel(data, "06-01-2023")
