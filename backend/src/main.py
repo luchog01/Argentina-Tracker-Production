57cb48db491bb326abe08a82004052876569c818
Luciano Garbarino <lucigar01@gmail.com>
2024-01-10 21:35:38 -0300

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -12,6 +12,7 @@ from datetime import datetime
 import json
 from cachetools import TTLCache, cached
 import os
+import uvicorn
 
 module_dir = os.path.dirname(__file__)  # get current directory
 users_file = os.path.join(module_dir, 'users.json')
